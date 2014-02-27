2f857d9e12adb81fb88a39a05025d83d7f4066f7
wreynolds <wreynolds@d5154090-6652-2845-be30-93a5ca2c7496>
2014-02-27 18:21:13 +0000

    Correct Frog
    git-svn-id: https://svn.neumont.edu:8443/svn/csc150finalgroup7@20 d5154090-6652-2845-be30-93a5ca2c7496

--- Frogger/src/edu/neumont/csc150/finalproject/finalgroup7/Frog.java
+++ Frogger/src/edu/neumont/csc150/finalproject/finalgroup7/Frog.java
@@ -6,59 +6,23 @@ import java.awt.Toolkit;
 
 public class Frog extends Sprite {
 	private Image[] imgArray;
-
-<<<<<<< .mine
-=======
 	private Image imageUp;
 	private Image imageLeft;
 	private Image imageRight;
 	private Image imageDown;
 
->>>>>>> .r18
-<<<<<<< .mine
-	public Frog(Point startPosition, boolean movesToTheLeft, int spriteSpeed, String imagePath) {
-		super(startPosition, movesToTheLeft, spriteSpeed, imagePath);
+	public Frog(Point startPosition, int spriteSpeed, String imagePath) {
+		super(startPosition, spriteSpeed, imagePath);
 		
 		imgArray = new Image[4];
 		imgArray[0] = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + "moving left.png");
 		imgArray[1] = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + "moving up.png");
 		imgArray[2] = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + "moving right.png");
 		imgArray[3] = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + "moving down.png");
-=======
-	public Frog(Point startPosition, int spriteSpeed, String imagePath) {
-		super(startPosition, spriteSpeed, imagePath);
-		this.imageUp = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + imagePath);
->>>>>>> .r18
 	}
-<<<<<<< .mine
 	
 	public void setImage(int imageNum){
 		this.image = imgArray[imageNum];
 	}
-	
-	
-=======
-	
-	public void addAltImages(String left, String right, String down) {
-		this.imageLeft = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + left);
-		this.imageRight = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + right);
-		this.imageDown = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + down);
-	}
 
-	public void setImageUp() {
-		super.image = this.imageUp;
-	}
-
-	public void setImageDown() {
-		super.image = this.imageDown;
-	}
-
-	public void setImageLeft() {
-		super.image = this.imageLeft;
-	}
-
-	public void setImageRight() {
-		super.image = this.imageRight;
-	}
->>>>>>> .r18
 }
