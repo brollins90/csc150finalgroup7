6722efd0a2347fb480b490a23beacb2ee856c02d
wreynolds <wreynolds@d5154090-6652-2845-be30-93a5ca2c7496>
2014-02-28 18:10:56 +0000

    Turtle class can now float and sink
    git-svn-id: https://svn.neumont.edu:8443/svn/csc150finalgroup7@29 d5154090-6652-2845-be30-93a5ca2c7496

--- Frogger/src/edu/neumont/csc150/finalproject/finalgroup7/Turtle.java
+++ Frogger/src/edu/neumont/csc150/finalproject/finalgroup7/Turtle.java
@@ -7,24 +7,43 @@ import java.awt.Toolkit;
 public class Turtle extends Enemy {
 	protected Image goodImage;
 	protected Image badImage;
+	protected Image sinkImage;
+	protected Image surfaceImage;
 	private int sinkTimer;
 	
 	public Turtle(Point startPosition, int spriteSpeed, String imagePath, String imagePathBad) {
 		super(startPosition, spriteSpeed, imagePath);
 		this.goodImage = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + imagePath);
 		this.badImage = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + imagePathBad);
+		this.sinkImage = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + "Wiggler mad sinking.png");
+		this.surfaceImage = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(".").getPath() + "Wiggler surfacing.png");
 		this.sinkTimer = 0;
 	}
 	
 	@Override
 	public void move() {
-		super.move();		
-		if(sinkTimer<25){
-			sinkTimer++;
-		}
-		if(sinkTimer == 25){
+		super.move();
+		sinkTimer++;
+		if(sinkTimer==24){
 			super.image = this.badImage;
+		}
+		if(sinkTimer==26){
+			super.image = this.sinkImage;
+		}
+		if(sinkTimer==28){
+			super.image = null;
 			super.isEvil = true;
+		}
+		
+		if(sinkTimer == 42){
+			super.image = this.surfaceImage;
+			super.isEvil = false;
+
+		}
+		
+		if(sinkTimer==44){
+			super.image = this.goodImage;
+			sinkTimer=0;
 		}
 	}
 	
