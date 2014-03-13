05c7c153bb6430996cac53c5edadb8d77aea6563
brollins <brollins@d5154090-6652-2845-be30-93a5ca2c7496>
2014-03-13 17:18:18 +0000

    fixed bad controller merge issue
    git-svn-id: https://svn.neumont.edu:8443/svn/csc150finalgroup7@58 d5154090-6652-2845-be30-93a5ca2c7496

--- Frogger/src/edu/neumont/csc150/finalproject/finalgroup7/Controller.java
+++ Frogger/src/edu/neumont/csc150/finalproject/finalgroup7/Controller.java
@@ -5,12 +5,11 @@ public class Controller {
 	public static void main(String[] args) {
 
 		// Open the Map Loader Panel
-//		Config currentMap = new Config("lvl01.xml");
+		Config currentMap = new Config("lvl01.xml");
 
-		GameFrame f = new GameFrame();
-		// // Start main frogger game with selected map
-//		Game currentGame = new Game(currentMap);
-//		currentGame.play();
+		// Start main frogger game with selected map
+		Game currentGame = new Game(currentMap);
+		currentGame.play();
 
 	}
 }
