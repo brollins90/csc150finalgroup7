10c8f4e74d806559defca0153081880393085726
brollins <brollins@d5154090-6652-2845-be30-93a5ca2c7496>
2014-03-14 15:56:27 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/csc150finalgroup7@69 d5154090-6652-2845-be30-93a5ca2c7496

---
+++ Frogger/src/edu/neumont/csc150/finalproject/finalgroup7/ConfigurationException.java
@@ -0,0 +1,10 @@
+package edu.neumont.csc150.finalproject.finalgroup7;
+
+public class ConfigurationException extends Exception {
+
+	private static final long serialVersionUID = 19L;
+	
+	public ConfigurationException(String message) {
+		super(message);
+	}
+}
